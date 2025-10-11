c2966d42fee59701abc0e128e062ce394baa53fc
Thamizh <thamizh@happy.local>
2025-10-11 13:37:13 +0530

    working on dashboard

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,7 +1,10 @@
+import GoogleTextInput from "@/components/GoogleTextInput";
+import Map from "@/components/Map";
 import RideCard from "@/components/RideCard";
+import { icons, images } from "@/constants";
 import { useUser } from "@clerk/clerk-expo";
 import { useEffect } from "react";
-import { FlatList, Text, View } from "react-native";
+import { ActivityIndicator, FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 // A small utility to ensure logs only run in development.
@@ -119,6 +122,10 @@ const recentRides = [
 
 export default function Home() {
   const { isLoaded, user, isSignedIn } = useUser();
+  const loading = true;
+
+  const handleSignOut = () => {};
+  const handleDestinationPress = () => {};
 
   useEffect(() => {
     log("Home component mounted!");
@@ -174,8 +181,65 @@ export default function Home() {
   return (
     <SafeAreaView className="bg-general-500">
       <FlatList
+        // data={[]}
         data={recentRides?.slice(0, 5)}
         renderItem={({ item }: any) => <RideCard ride={item} />}
+        keyExtractor={(item, index) => index.toString()}
+        className="px-5"
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+        ListEmptyComponent={() => (
+          <View className="flex flex-col justify-center items-center">
+            {!loading ? (
+              <>
+                <Image
+                  className="w-40 h-40"
+                  source={images.noResult}
+                  alt="No recent rides found"
+                  resizeMode="contain"
+                />
+                <Text className="text-sm">No recent rides found</Text>
+              </>
+            ) : (
+              <View className="flex flex-row justify-center items-center  ">
+                <ActivityIndicator size="large" color="#000" />
+              </View>
+            )}
+          </View>
+        )}
+        ListHeaderComponent={() => (
+          <>
+            <View className="flex flex-row items-center justify-between my-5">
+              <Text className="text-2xl font-JakartaExtraBold">
+                Welcome {user?.firstName}{" "}👋
+              </Text>
+              <TouchableOpacity
+                onPress={handleSignOut}
+                className="justify-center items-center w-10 h-10 rounded-full bg-white"
+              >
+                <Image source={icons.out} className="w-4 h-4" />
+              </TouchableOpacity>
+            </View>
+
+            <GoogleTextInput
+              icon={icons.search}
+              containerStyle="bg-white shadow-md shadow-neutral-300"
+              handlePress={handleDestinationPress}
+            />
+
+          <>
+          <Text className="text-xl font-JarkartaBold mt-5 mb-3">Your Current Location</Text>
+          <View className="flex flex-row items-center bg-transparent h-[300px]">
+          <Map />
+          </View>
+          </>
+          
+          <Text className="text-xl font-JakartaBold mt-5 mb-3">Recent Rides</Text>
+
+          </>
+        )}
       />
     </SafeAreaView>
   );
---
+++ components/Map.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+const Map = () => (
+    <View>
+        <Text>Map Component here</Text>
+    </View>
+);
+
+export default Map;
